64d5433abf549b14a610c202fbf9638c42df2da6
Mutasim Sweileh <mutasimsweileh@gmail.com>
2023-12-09 08:05:40 +0200

    fix and improve code

--- app.py
+++ app.py
@@ -7,45 +7,57 @@ load_dotenv()
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
 # Define a function to interact with the chatbot
-def chat_with_bot(message):
-    # Define the initial user message
-    user_message = message
 
-    # Initialize the chat history
-    chat_history = user_message
 
-    while True:
-        # Call the OpenAI ChatCompletion API
-        response = openai.Completion.create(
-            engine='davinci',
-            prompt=chat_history,
-            max_tokens=100,
-            temperature=0.6,
-            n=1,
-            stop=None,
-            temperature=0.6
-        )
+class ChatBot:
+    def __init__(self):
+        self.chat_messages = [
+            {"role": "system", "content": "You are a helpful assistant."}
+        ]
 
-        # Get the model's reply from the response
-        model_reply = response.choices[0].text.strip()
+    def ask(self, message):
+        # Define the initial user message
+        user_message = message
+        chat_messages = self.chat_messages
+        # Initialize the chat history
+        chat_history = user_message
+        while True:
+            chat_messages.append({"role": "user", "content": user_message})
+            # print(chat_messages)
+            # Call the OpenAI ChatCompletion API
+            try:
+                response = openai.chat.completions.create(
+                    model="gpt-3.5-turbo",
+                    messages=chat_messages,
+                    max_tokens=100,
+                    temperature=0.6
+                )
+            except Exception as e:
+                print("Chat Error: ", str(e))
+                return
 
-        # Append the model's reply to the chat history
-        chat_history += model_reply
+            # Get the model's reply from the response
+            model_reply = response.choices[0].message
+            chat_messages.append(model_reply)
+            # Append the model's reply to the chat history
+            chat_history += model_reply.content
 
-        # Print the model's reply
-        print('Bot:', model_reply)
+            # Print the model's reply
+            print('Bot:', model_reply.content)
 
-        # Get the user's next message
-        user_message = input('You: ')
+            # Get the user's next message
+            user_message = input('You: ')
 
-        # Append the user's message to the chat history
-        chat_history += '\nUser: ' + user_message
+            # Append the user's message to the chat history
+            chat_history += '\nUser: ' + user_message
+            self.chat_messages = chat_messages
+            # Check if the user wants to exit
+            if user_message.lower() == 'exit':
+                break
 
-        # Check if the user wants to exit
-        if user_message.lower() == 'exit':
-            break
 
-# Start the chatbot
-print("Welcome to the Chatbot! Type 'exit' to end the conversation.")
-user_input = input('You: ')
-chat_with_bot(user_input)
+if __name__ == '__main__':
+    print("Welcome to the Chatbot! Type 'exit' to end the conversation.")
+    user_input = input('You: ')
+    chat = ChatBot()
+    chat.ask(user_input)
